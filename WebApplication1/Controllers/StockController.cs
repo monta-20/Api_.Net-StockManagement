ba873d843946e7824400f2e1b1abc97d330ab43f
monta-20 <montassarbenbrahim29@gmail.com>
2025-05-10 14:13:01 +0100

    get list and details from Stock

---
+++ WebApplication1/Controllers/StockController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+    
+{
+    [Route("api/stock")]
+    public class StockController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public StockController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stock.ToList();
+            return Ok(stocks);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stock.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+
+        }
+    }
+
+
+}
